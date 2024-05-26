0e7ed89173e3afe2c13e8df675ef42bbc2880832
SamuelJucker <133858501+Samilu02@users.noreply.github.com>
2024-05-26 05:41:30 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-#cgeck
-#example
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
